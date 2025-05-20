0c63a59d87cb106d00d7a10d7b49b2ee42663549
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-05-20 15:14:02 +0200

    added Passerotto details

--- src/animals/Passerotto.java
+++ src/animals/Passerotto.java
@@ -1,6 +1,24 @@
 package animals;
 
 public class Passerotto extends Animale {
+    // characteristics
+    protected String color;
+
+    // getters and setters
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setName(String color) {
+        this.color = color;
+    }
+
+    // constructor
+    public Passerotto(String name, String color) {
+        super(name);
+        this.color = color;
+    }
+
     @Override
     public void verso() {
         System.out.println("cinguetta");
