21aa76bfb39531ee6aedd717a104f0711eb42198
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-05-20 15:03:40 +0200

    added characteristis, getters/setters and constructor to Animale

--- src/animals/Animale.java
+++ src/animals/Animale.java
@@ -1,6 +1,24 @@
 package animals;
 
 public abstract class Animale {
+    // characteristics
+    protected String name;
+
+    // getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // constructor
+    public Animale(String name) {
+        this.name = name;
+    }
+
+    // methods
     public void dormi() {
         System.out.println("Zzzz");
     };
