61548030738875e6689c7e787539e0bb8bf6f6c4
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-05-20 15:17:59 +0200

    added Delfino details

--- src/animals/Delfino.java
+++ src/animals/Delfino.java
@@ -1,6 +1,24 @@
 package animals;
 
 public class Delfino extends Animale {
+    // characteristics
+    protected String subspecies;
+
+    // getters and setters
+    public String getSubspecies() {
+        return this.subspecies;
+    }
+
+    public void setSubspecies(String subspecies) {
+        this.subspecies = subspecies;
+    }
+
+    // constructor
+    public Delfino(String name, String subspecies) {
+        super(name);
+        this.subspecies = subspecies;
+    }
+
     @Override
     public void verso() {
         System.out.println("fischia");
