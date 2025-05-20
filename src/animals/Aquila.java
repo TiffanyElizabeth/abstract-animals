f4b3be9d0424950c60b7ce6b2c68a02af5f7beb1
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-05-20 15:17:01 +0200

    added Aquila details

--- src/animals/Aquila.java
+++ src/animals/Aquila.java
@@ -1,6 +1,24 @@
 package animals;
 
 public class Aquila extends Animale {
+    // characteristics
+    protected String subspecies;
+
+    // getters and setters
+    public String getSubspecies() {
+        return this.subspecies;
+    }
+
+    public void setSubspecies(String subspecies) {
+        this.subspecies = subspecies;
+    }
+
+    // constructor
+    public Aquila(String name, String subspecies) {
+        super(name);
+        this.subspecies = subspecies;
+    }
+
     @Override
     public void verso() {
         System.out.println("grida");
