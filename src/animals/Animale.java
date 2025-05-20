dd394b0c3eda8c4cbcaa0a2f38d7781ec5c49e03
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-05-20 09:20:09 +0200

    add methods to Animale

--- src/animals/Animale.java
+++ src/animals/Animale.java
@@ -1,5 +1,12 @@
 package animals;
 
 public abstract class Animale {
+    public void dormi() {
+        System.out.println("Zzzz");
+    };
+
+    public abstract void verso();
+
+    public abstract void mangia();
 
 }
